32c2fc069ef62ed2d2f547a5751bf65b2f537480
AbhishekHazraIITB26 <126457025+AbhishekHazraIITB26@users.noreply.github.com>
2023-05-14 23:45:44 +0530

    Qstn1(B).py

--- Qstn1(B).py
+++ Qstn1(B).py
@@ -1,14 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 url = 'https://itc.gymkhana.iitb.ac.in/wncc/soc/'
 html_text= requests.get(url).text
 soup = BeautifulSoup(html_text, 'lxml')
 Project_names=[]
-Mentors=[]
-Mentees=[]
-Descriptions=[]
-Resources_Prereqs=[]
-i=0
+Mentors_Mentees=[]
+c=[]
 # loop through all URLs and extract data from each file
 for link in soup.find_all('a'):
     href = link.get('href')
@@ -16,6 +14,21 @@ for link in soup.find_all('a'):
         href='https://itc.gymkhana.iitb.ac.in'+href
         html_text2 = requests.get(href).text
         soup1=BeautifulSoup(html_text2,'lxml')
-        Project_names.append(soup1.find('h2',class_='display1 m-3 p-3 text-center project-title').text)        
-print(Project_names)
+        Project_names.append(soup1.find('h2',class_='display1 m-3 p-3 text-center project-title').text)
+        for i in soup1.find_all("li"):
+          for j in i.find_all("p",class_="lead"):
+               Mentors_Mentees.append(j.get_text())
+    x=len(Mentors_Mentees)
+        
+    if x>1:
+        l=""
+        for f in Mentors_Mentees:
+            l=l+f+" "+","
+        c.append(l)
+    
+    Mentors_Mentees.clear()
+Final_Tuple=list(zip(Project_names,c))
+df=pd.DataFrame(Final_Tuple, columns=["NAME OF PROJECT","MENTORS, NUMBER OF MENTEES"])
+df.to_csv("Abhishek Hazra.csv")
+
  

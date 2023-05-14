ac115c6c1090218751abc94956a01d305248c10e
AbhishekHazraIITB26 <126457025+AbhishekHazraIITB26@users.noreply.github.com>
2023-05-14 18:24:37 +0530

    Qstn1.cpp

--- Qstn1.cpp
+++ Qstn1.cpp
@@ -1,205 +1,90 @@
 #include <iostream>
-#include<cstdlib>
-#include <algorithm>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
-int fact(int n)
-{
-    if (n == 1 || n == 0)
-        return 1;
-    else
-        return n * fact(n - 1);
+
+bool hasMentor(unordered_map<int, vector<int>>& participants, const string& project, int rollNumber) {
+    vector<int>& currentSkills = participants[rollNumber];
+    for (auto& participant : participants) {
+        if (participant.first != rollNumber && participant.second == currentSkills) {
+            if (participant.first == rollNumber)
+                return true;
+        }
+    }
+    return false;
 }
-int main()
-{
-    int n=7;
-    int skills1[n][5]={{1,0,3,0,1},{2,3,0,1,1},{1,0,0,1,1},{0,1,2,1,1},{2,0,0,1,0},{1,2,0,3,1},{1,2,2,0,1}};
-    string roll[n];
-    int m=2;
-    string ProjectName[m];
-    int Projects[m][5]={{1,1,2,0,0},{0,0,2,3,1}};
 
-    int skills[5 * m][5] = {0,0,0,0,0};
-    int parNos1[5 * m] = {0};
-    int completions[fact(5 * m)] = {0};
-    int completions1[fact(n)] = {0};
-    if (n < (5 * m))
-    {
-        for (int i = 0; i < 5 * m; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                skills[i][j] = skills1[i][j];
-            }
-        }
-        for (int i = 0; i < 5 * m; i++)
-        {
-            parNos1[i] = i;
-        }
-        int **permutations = new int *[fact(5*m)];
-        for (int i = 0; i < fact(5*m); ++i)
-        {
-            permutations[i] = new int[5 * m];
-        }
-        int *numbers = new int[5 * m];
-        for (int i = 0; i < 5 * m; ++i)
-        {
-            numbers[i] = i;
-        }
-
-        int row = 0;
-        do
-        {
-            for (int i = 0; i < 5 * m; ++i)
-            {
-                permutations[row][i] = numbers[i];
-            }
-            ++row;
-        } while (next_permutation(numbers, numbers + 5 * m));
-        for (int i = 0; i < fact(5*m); i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                for (int k = 0; k < 5 * m; k = k + 5)
-                {
-                    if (Projects[j][0] - skills[permutations[i][k]][0] >= 2 || Projects[j][1] - skills[permutations[i][k + 1]][1] >= 2 || Projects[j][2] - skills[permutations[i][k + 2]][2] >= 2 || Projects[j][3] - skills[permutations[i][k + 3]][3] >= 2 || Projects[j][4] - skills[permutations[i][k + 4]][4] >= 2)
-                        continue;
-                    else
-                    {
-                        if (skills[permutations[i][k]][0] >= Projects[j][0] && skills[permutations[i][k + 1]][1] >= Projects[j][1] && skills[permutations[i][k + 2]][2] >= Projects[j][2] && skills[permutations[i][k + 3]][3] >= Projects[j][3] && skills[permutations[i][k + 4]][4] >= Projects[j][4])
-                        {
-                        completions[i]++;
-                        }
-                        else 
-                        {
-                            // Create a dynamically allocated 2D array
-                            int **arr = new int *[fact(5)];
-                            for (int i = 0; i < fact(5); ++i)
-                            {
-                                arr[i] = new int[5];
-                            }
-                            // Generate permutations
-                            int *numbers = new int[5];
-                            for (int i = 0; i < 5; ++i)
-                            {
-                                numbers[i] = i;
-                            }
-
-                            int row = 0;
-                            do
-                            {
-                                for (int i = 0; i < 5; ++i)
-                                {
-                                    arr[row][i] = numbers[i];
-                                }
-                                ++row;
-                            } while (next_permutation(numbers, numbers + 5));
-
-                            for (int a = 0;a<120;a++)
-                            {
+int main() {
+    unordered_map<int, vector<int>> participants;
+    unordered_set<int> joinedProjects; // To track projects already joined by a participant
+    int N, M;
+    cout << "Enter the number of participants: ";
+    cin >> N;
     
-                                if(skills[arr[a][0]][0]-skills[arr[a][1]][0]<=1&&skills[arr[a][1]][1]-skills[arr[a][2]][1]<=1&&skills[arr[a][2]][2]-skills[arr[a][3]][2]<=1&&skills[arr[a][3]][3]-skills[arr[a][4]][3]<=1&&skills[arr[a][4]][4]-skills[arr[a][0]][4]<=1&&skills[arr[a][0]][0]-skills[arr[a][1]][0]>=0&&skills[arr[a][1]][1]-skills[arr[a][2]][1]>=0&&skills[arr[a][2]][2]-skills[arr[a][3]][2]>=0&&skills[arr[a][3]][3]-skills[arr[a][4]][3]>=0&&skills[arr[a][4]][4]-skills[arr[a][0]][4]>=0)
-                                {
-                                completions[i]++;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+    for (int i = 0; i < N; i++) {
+        int rollNumber;
+        vector<int> skillLevels(5);
+        cout << "Enter participant's roll number followed by skill levels (HTML, Python, DSA, Java, SQL): ";
+        cin >> rollNumber;
+        for (int j = 0; j < 5; j++) {
+            cin >> skillLevels[j];
         }
+        participants[rollNumber] = skillLevels;
     }
-    else
-    {
-        // Create a dynamically allocated 2D array
-        int **permutations = new int *[fact(n)];
-        for (int i = 0; i < fact(n); ++i)
-        {
-            permutations[i] = new int[n];
+    
+    int completedProjects = 0;
+    cout << "Enter the number of projects: ";
+    cin >> M;
+    
+    for (int i = 0; i < M; i++) {
+        string projectName;
+        vector<int> skillLevelsRequired(5);
+        cout << "Enter project name followed by required skill levels (HTML, Python, DSA, Java, SQL): ";
+        cin >> projectName;
+        for (int j = 0; j < 5; j++) {
+            cin >> skillLevelsRequired[j];
         }
-
-        // Generate permutations
-        int *numbers = new int[n];
-        for (int i = 0; i < n; ++i)
-        {
-            numbers[i] = i;
-        }
-
-        int row = 0;
-        do
-        {
-            for (int i = 0; i < n; ++i)
-            {
-                permutations[row][i] = numbers[i];
+        bool projectCompleted = true;
+        
+        for (int j = 0; j < 5; j++) {
+            int skillRequired = skillLevelsRequired[j];
+            if (skillRequired == 0) {
+                continue;
             }
-            ++row;
-        } while (next_permutation(numbers, numbers + n));
-        for (int i = 0; i < fact(n); i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                for (int k = 0; k < 5 * m; k = k + 5)
-                {
-                    if (Projects[j][0] - skills1[permutations[i][k]][0] >= 2 || Projects[j][1] - skills1[permutations[i][k + 1]][1] >= 2 || Projects[j][2] - skills1[permutations[i][k + 2]][2] >= 2 || Projects[j][3] - skills1[permutations[i][k + 3]][3] >= 2 || Projects[j][4] - skills1[permutations[i][k + 4]][4] >= 2)
+            bool skillFound = false;
+            
+            for (auto& participant : participants) {
+                int rollNumber = participant.first;
+                // Skip participant if already joined a project
+                if (joinedProjects.count(rollNumber) > 0)
                     continue;
-                    else
-                    {
-                        if (skills1[permutations[i][k]][0] >= Projects[j][0] && skills1[permutations[i][k + 1]][1] >= Projects[j][1] && skills1[permutations[i][k + 2]][2] >= Projects[j][2] && skills1[permutations[i][k + 3]][3] >= Projects[j][3] && skills1[permutations[i][k + 4]][4] >= Projects[j][4])
-                        completions1[i]++;
-                        else 
-                        {
-
-                            // Create a dynamically allocated 2D array
-                            int **arr = new int *[fact(n)];
-                            for (int i = 0; i < fact(n); ++i)
-                            {
-                                arr[i] = new int[5];
-                            }
-
-                            // Generate permutations
-                            int *numbers = new int[5];
-                            for (int i = 0; i < 5; ++i)
-                            {
-                                numbers[i] = i;
-                            }
-
-                            int row = 0;
-                            do
-                            {
-                                for (int i = 0; i < 5; ++i)
-                                {
-                                    arr[row][i] = numbers[i];
-                                }
-                                ++row;
-                            } while (next_permutation(numbers, numbers + 5));
-
-                            for (int a = 0;a<120;a++)
-                            {
-                                if(skills1[arr[a][0]][0]-skills1[arr[a][1]][0]<=1&&skills1[arr[a][1]][1]-skills1[arr[a][2]][1]<=1&&skills1[arr[a][2]][2]-skills1[arr[a][3]][2]<=1&&skills1[arr[a][3]][3]-skills1[arr[a][4]][3]<=1&&skills1[arr[a][4]][4]-skills1[arr[a][0]][4]<=1&&skills1[arr[a][0]][0]-skills1[arr[a][1]][0]>=0&&skills1[arr[a][1]][1]-skills1[arr[a][2]][1]>=0&&skills1[arr[a][2]][2]-skills1[arr[a][3]][2]>=0&&skills1[arr[a][3]][3]-skills1[arr[a][4]][3]>=0&&skills1[arr[a][4]][4]-skills1[arr[a][0]][4]>=0)
-                                completions1[i]++;
-                            }
-                        }
+                
+                if (participant.second[j] >= skillRequired) {
+                    skillFound = true;
+                    joinedProjects.insert(rollNumber); // Mark participant as joined
+                    break;
+                } else if (participant.second[j] == skillRequired - 1) {
+                    if (hasMentor(participants, projectName, rollNumber)) {
+                        skillFound = true;
+                        joinedProjects.insert(rollNumber); // Mark participant as joined
+                        break;
                     }
                 }
             }
+            
+            if (!skillFound) {
+                projectCompleted = false;
+                break;
+            }
         }
+        
+        if (projectCompleted) {
+            completedProjects++;
+        }
+    }
     
-    }
-    int completions2[fact(5*m)+fact(n)];
-    for(int i=0;i<fact(5*m)+fact(n);i++)
-    {
-        if(i<fact(5*m))
-        completions2[i]=completions[i];
-        else
-        completions2[i]=completions1[i-fact(5*m)];
-    }
-    int max_projects=0;
-    for(int i=0;i<fact(5*m)+fact(n);i++)
-    {
-        if(completions2[i]>max_projects)
-        max_projects=completions2[i];
-    }
-    cout << max_projects;
+    cout << "Number of completed projects: " << completedProjects << endl;
+    
     return 0;
 }
-
-
